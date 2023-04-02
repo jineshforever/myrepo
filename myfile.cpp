5df121e70062e113d2857c22c1d1753adfe7094a
jinesh nadar <jineshforever@gmail.com>
2023-04-02 23:25:28 +0530

    added new file

---
+++ myfile.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void func(int *arr, int count)
+{
+    cout << "Hi";
+    cout << "Hello";
+}
+
+void bubblesort()
+{
+}
+
+void quicksort()
+{
+}
+
+int main()
+{
+    int n = 3;
+    int arr[]{};
+    func(arr, n);
+    return 0;
+}
